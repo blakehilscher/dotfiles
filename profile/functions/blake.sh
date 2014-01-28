e062546e44f548ba67c498fbb06944d5e7e33369
Blake Hilscher <blake@hilscher.ca>
2014-01-28 11:32:19 -0500

    rename quandl to blake.

--- profile/functions/blake.sh
+++ profile/functions/blake.sh
@@ -1,3 +1,39 @@
+function g()
+{
+  case $1 in
+  c)
+    cd ~/chef/chef-repo/;;
+  cm)
+    cd ~/chef/11/;;
+  g)
+    cd ~/www/gems;;
+  gq)
+    cd ~/www/gems/quandl;;
+
+  qc)
+    cd ~/www/gems/quandl_cassandra;;
+  qcm)
+    cd ~/www/gems/quandl_cassandra_models;;
+  h)
+    cd ~/www/hilscher/;;
+  n)
+    cd /opt/nginx/;;
+  q)
+    cd ~/www/quandl/;;
+  w)
+    cd ~/www/quandl/wikiposit/;;
+  wu)
+    cd ~/www/quandl/wikiposit-utilities/;;
+  wc)
+    cd ~/www/quandl/wikiposit_cassandra/;;
+  s)
+    cd ~/user_settings/;;
+  esac
+  if [ $2 ];then
+    cd $2
+  fi
+}
+
 function quandl_ssh_cassandra () {
   NAME="$1-cassandra"
   split_pane_vertical "knife into $NAME-1.2"
--- profile/functions/general.sh
+++ profile/functions/general.sh
@@ -1,42 +1,4 @@
-function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
 
-function g()
-{
-  case $1 in
-  c)
-    cd ~/chef/chef-repo/;;
-  cm)
-    cd ~/chef/11/;;
-  g)
-    cd ~/www/gems;;
-  gq)
-    cd ~/www/gems/quandl;;
-
-  qc)
-    cd ~/www/gems/quandl_cassandra;;
-  qcm)
-    cd ~/www/gems/quandl_cassandra_models;;
-  h)
-    cd ~/www/hilscher/;;
-  n)
-    cd /opt/nginx/;;
-  q)
-    cd ~/www/quandl/;;
-  w)
-    cd ~/www/quandl/wikiposit/;;
-  wu)
-    cd ~/www/quandl/wikiposit-utilities/;;
-  wc)
-    cd ~/www/quandl/wikiposit_cassandra/;;
-  s)
-    cd ~/user_settings/;;
-  esac
-  if [ $2 ];then
-    cd $2
-  fi
-}
 function bg_exec() {
   LOG="$HOME/logs/bg_exec.log"
   eval "$1 >> $LOG &"
