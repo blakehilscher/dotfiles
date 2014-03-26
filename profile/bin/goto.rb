f1ae1ce3d5b5e1ca2aaa6b0e7e5f2dc34110192e
Blake Hilscher <blake@hilscher.ca>
2014-03-26 13:19:47 -0400

    add expressions

--- profile/bin/goto.rb
+++ profile/bin/goto.rb
@@ -1,23 +1,49 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
-Dir.chdir(ENV['HOME'])
-
-ARGV.each do |match|
-  dirs = Dir['*'].select{|d| File.directory?(d) }.collect(&:downcase)
-  dirs = dirs.collect{|s| s.gsub(/[-_ ]+/,'')}.sort_by{|k,v| k.length }
-  dirs = dirs.select{|k| k =~ /#{match}/ } if dirs.count > 1
-  
-  if dirs.count > 1
-    chars = dirs.select{|d| d[0] == match[0] } 
-    dirs = chars unless chars.length == 0
+def goto(*args)
+  args.each_with_index do |match, index|
+    if match[0] == '-'
+      goto_expression(match)
+    else
+      goto_dir(match)
+    end
   end
-  location = dirs.first
+  print `pwd`
+end
+
+def goto_expression(exp)
+  require 'yaml'
+  dir = expressions[exp[1..-1]]
+  unless dir.nil?
+    dir.gsub!(/^\~/, ENV['HOME'])
+    Dir.chdir( dir )
+  else
+    raise "no expression #{exp} in (#{expressions.keys.join(', ')})"
+  end
+end
+
+def goto_dir(match)
+  
+  dirs = Dir['*'].select{|d| File.directory?(d) }.sort_by{|k,v| k.length }
+  dirs_hash = dirs.inject({}){|m,d| m[d] = d.downcase.gsub(/[-_ ]+/,''); m }
+  dirs_hash = dirs_hash.select{|k,v| v =~ /#{match}/ }
+  
+  if dirs_hash.count > 1
+    chars = dirs_hash.select{|k,v| v[0] == match[0] } 
+    dirs_hash = chars unless chars.length == 0
+  end
+  location = dirs_hash.keys.first
   if location.nil?
     raise "nothing matched #{match} in #{`pwd`}"
   else
     Dir.chdir(location)
   end
+  
 end
 
-print `pwd`
+def expressions
+  @expressions ||= YAML.load(File.read(File.join(ENV['HOME'], '.goto') ))
+end
+
+goto(*ARGV)
