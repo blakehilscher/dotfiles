9c480030950fba8c0a7605065e36d29a5cc3981d
Blake Hilscher <blake@hilscher.ca>
2014-06-12 12:01:25 -0400

    junk

--- customize/customize_profile.sh
+++ customize/customize_profile.sh
@@ -1,10 +1,10 @@
-export BLUE="\[\033[0;34m\]"
-export RED="\[\033[0;31m\]"
-export LIGHT_RED="\[\033[1;31m\]"
-export GREEN="\[\033[0;32m\]"
-export YELLOW="\[\033[0;33m\]"
-export LIGHT_GREEN="\[\033[1;32m\]"
-export WHITE="\[\033[1;37m\]"
-export LIGHT_GRAY="\[\033[0;37m\]"
+export COLOR_BLUE="\[\033[0;34m\]"
+export COLOR_RED="\[\033[0;31m\]"
+export COLOR_LIGHT_RED="\[\033[1;31m\]"
+export COLOR_GREEN="\[\033[0;32m\]"
+export COLOR_YELLOW="\[\033[0;33m\]"
+export COLOR_LIGHT_GREEN="\[\033[1;32m\]"
+export COLOR_WHITE="\[\033[1;37m\]"
+export COLOR_LIGHT_GRAY="\[\033[0;37m\]"
 
 export PROMPT_COLOR=$YELLOW
