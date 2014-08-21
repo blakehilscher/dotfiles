caa20bda3c2822aff5b664790e400bc121d70908
Blake Hilscher <blake@hilscher.ca>
2014-08-21 10:17:41 -0400

    add gi for gitignore.io support

--- profile/functions/git.sh
+++ profile/functions/git.sh
@@ -1,3 +1,8 @@
 function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
+
+
+function gi() {
+  curl http://www.gitignore.io/api/$@ > .gitignore
+}
